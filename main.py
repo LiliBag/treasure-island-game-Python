6a377c62abcb006f770f53d6cd99f099b7cfe524
LiliBag <lishilaya@gmail.com>
2021-05-28 14:56:39 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+firstInput = input("Right or Left?")
+if firstInput=="Left" or firstInput=="left":
+  secondInput= input("You chose the right way. Now youre at the lake. Swim or Wait?")
+  if secondInput=="Wait" or secondInput=="wait":
+    thirdInput= input("You waited and all of the sudden a door appears.Then another and then another one! Which door would you choose? Red, Blue or Yellow?")
+    if thirdInput=="Red" or thirdInput=="red":
+      print("Birned by fire! Game over. Lever 3 achived.")
+    elif thirdInput=="Blue" or thirdInput=="blue":
+      print("You are eaten by beasts! Game over! Level 3 achieved.")
+    elif thirdInput=="Yellow" or thirdInput=="yellow":
+      print("You won!")
+    else:
+      print("You lost. Level 3 achieved.")
+  else: 
+    print("Game Over. Level 2 achived.")
+else:
+  print("Game over. Level 1.")
